d3a2e3a327819c02dbaf917bca541091ffc3ed78
Igor Lesik <ilesik@tachyum.com>
2023-03-22 12:18:54 -0700

    placeholder for MKS units

--- src/constant/scale.rs
+++ src/constant/scale.rs
@@ -5,7 +5,31 @@
 //!
 
 /// Dimensionless scaling factors
-pub trait Scale {
+pub trait Scale: Copy + core::ops::Mul<Output = Self> +  core::ops::Div<Output = Self> {
+
+    /// Scale a number by factor.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use rustamath::constant::scale::{Scale};
+    /// assert_eq!(2.1f64.scale(f64::MEGA), 2100_000.0_f64)
+    /// ```
+    fn scale(&self, factor: Self) -> Self {
+        *self * factor
+    }
+
+    /// Divide a number by factor.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use rustamath::constant::scale::{Scale};
+    /// assert_eq!(2.1f64.scale(f64::MEGA).in_units(f64::KILO), 2100.0_f64)
+    /// ```
+    fn in_units(&self, factor: Self) -> Self {
+        *self / factor
+    }
 
     /// 10^24
     const YOTTA: Self;
@@ -22,21 +46,6 @@ pub trait Scale {
     /// 10^6
     const MEGA: Self;
     /// 10^3
-    const KILO: Self;
-
-}
-
-impl Scale for f64 {
-
-    const YOTTA: f64 = 1.0e24_f64;
-    const ZETTA: f64 = 1.0e21_f64;
-    const EXA:   f64 = 1.0e18_f64;
-    const PETA:  f64 = 1.0e15_f64;
-    const TERA:  f64 = 1.0e12_f64;
-    const GIGA:  f64 = 1.0e9_f64;
-    const MEGA:  f64 = 1.0e6_f64;
-
-    /// 10^3
     ///
     /// # Example
     ///
@@ -44,6 +53,40 @@ impl Scale for f64 {
     /// # use rustamath::constant::scale::{Scale};
     /// assert_eq!(f64::KILO, 1000.0_f64)
     /// ```
-    const KILO: f64 = 1.0e3_f64;
+    const KILO: Self;
+    /// -3
+    const MILLI: Self;
+    /// -6
+    const MICRO: Self;
+    /// -9
+    const NANO: Self;
+    /// -12
+    const PICO: Self;
+    /// -15
+    const FEMTO: Self;
+    /// -18
+    const ATTO: Self;
+    /// -21
+    const ZEPTO: Self;
+    /// -24
+    const YOCTO: Self;
+}
 
+impl Scale for f64 {
+    const YOTTA: f64 = 1.0e24_f64;
+    const ZETTA: f64 = 1.0e21_f64;
+    const EXA:   f64 = 1.0e18_f64;
+    const PETA:  f64 = 1.0e15_f64;
+    const TERA:  f64 = 1.0e12_f64;
+    const GIGA:  f64 = 1.0e9_f64;
+    const MEGA:  f64 = 1.0e6_f64;
+    const KILO:  f64 = 1.0e3_f64;
+    const MILLI: f64 = 1.0e-3_f64;
+    const MICRO: f64 = 1.0e-6_f64;
+    const NANO:  f64 = 1.0e-9_f64;
+    const PICO:  f64 = 1.0e-12_f64;
+    const FEMTO: f64 = 1.0e-15_f64;
+    const ATTO:  f64 = 1.0e-18_f64;
+    const ZEPTO: f64 = 1.0e-21_f64;
+    const YOCTO: f64 = 1.0e-24_f64;
 }
