f14840a88d18ad72396177fadabbd5f71fef0d95
Igor Lesik <ilesik@tachyum.com>
2023-03-23 19:05:23 -0700

    mks

--- src/constant/mks.rs
+++ src/constant/mks.rs
@@ -94,11 +94,14 @@ pub const BOLTZMANN_UNIT:              MksUnit = MksUnit {m:  2, k:  1, s: -2, a
 pub const MOLAR_GAS_UNIT:              MksUnit = MksUnit {m:  2, k:  1, s: -2, a: 0}; // kg m^2 / K mol s^2
 /// Standard gas volume
 pub const STANDARD_GAS_VOLUME_UNIT:    MksUnit = MksUnit {m:  3, k:  0, s:  0, a: 0}; // m^3 / mol
+/// One second of time
+pub const SECOND_UNIT:                 MksUnit = MksUnit {m:  0, k:  0, s:  1, a: 0}; // s
 /// One minute of time
 pub const MINUTE_UNIT:                 MksUnit = MksUnit {m:  0, k:  0, s:  1, a: 0}; // s
 //pub const HOUR_UNIT:                   MksUnit = MksUnit {m:  0, k:  0, s:  0, a: 0}; /* s */
 //pub const DAY_UNIT:                    MksUnit = MksUnit {m:  0, k:  0, s:  0, a: 0}; /* s */
 //pub const WEEK_UNIT:                   MksUnit = MksUnit {m:  0, k:  0, s:  0, a: 0}; /* s */
+//pub const METER_UNIT:                   MksUnit = MksUnit {m:  0, k:  0, s:  0, a: 0}; /* m */
 //pub const INCH_UNIT:                   MksUnit = MksUnit {m:  0, k:  0, s:  0, a: 0}; /* m */
 //pub const FOOT_UNIT:                   MksUnit = MksUnit {m:  0, k:  0, s:  0, a: 0}; /* m */
 //pub const YARD_UNIT:                   MksUnit = MksUnit {m:  0, k:  0, s:  0, a: 0}; /* m */
@@ -127,6 +130,7 @@ pub const MINUTE_UNIT:                 MksUnit = MksUnit {m:  0, k:  0, s:  1, a
 /// km/h dimentions is [m/s]
 pub const KILOMETERS_PER_HOUR_UNIT: MksUnit = MksUnit {m: 1, k: 0, s: -1, a: 0};
 //pub const KNOT:                        MksUnit = ( 0,  0,  0,  0); /* m / s */
+//pub const KILOGRAM:                    MksUnit = ( 0,  0,  0,  0);/* kg */
 //pub const POUND_MASS:                  MksUnit = ( 0,  0,  0,  0);/* kg */
 //pub const OUNCE_MASS:                  MksUnit = ( 0,  0,  0,  0); /* kg */
 //pub const TON:                         MksUnit = ( 0,  0,  0,  0); /* kg */
@@ -485,92 +489,127 @@ where
     const MOLAR_GAS: Self;
     /// Standard gas volume
     const STANDARD_GAS_VOLUME:Self;
+    /// One second of time
+    const SECOND: Self;
     /// One minute of time, 60s
     const MINUTE: Self;
-    //const HOUR:                   f64 = (3.6e3) /* s */
-    //const DAY:                    f64 = (8.64e4) /* s */
-    //const WEEK:                   f64 = (6.048e5) /* s */
-    //const INCH:                   f64 = (2.54e-2) /* m */
-    //const FOOT:                   f64 = (3.048e-1) /* m */
-    //const YARD:                   f64 = (9.144e-1) /* m */
-    //const MILE:                   f64 = (1.609344e3) /* m */
-    //const NAUTICAL_MILE: f64 = (1.852e3) /* m */
-    //const FATHOM: f64 = (1.8288e0) /* m */
-    //const MIL: f64 = (2.54e-5) /* m */
-    //const POINT: f64 = (3.52777777778e-4) /* m */
-    //const TEXPOINT: f64 = (3.51459803515e-4) /* m */
-    //const MICRON: f64 = (1e-6) /* m */
-    //const ANGSTROM: f64 = (1e-10) /* m */
-    //const HECTARE: f64 = (1e4) /* m^2 */
-    //const ACRE: f64 = (4.04685642241e3) /* m^2 */
-    //const BARN: f64 = (1e-28) /* m^2 */
-    //const LITER: f64 = (1e-3) /* m^3 */
-    //const US_GALLON: f64 = (3.78541178402e-3) /* m^3 */
-    //const QUART: f64 = (9.46352946004e-4) /* m^3 */
-    //const PINT: f64 = (4.73176473002e-4) /* m^3 */
-    //const CUP: f64 = (2.36588236501e-4) /* m^3 */
-    //const FLUID_OUNCE: f64 = (2.95735295626e-5) /* m^3 */
-    //const TABLESPOON: f64 = (1.47867647813e-5) /* m^3 */
-    //const TEASPOON: f64 = (4.92892159375e-6) /* m^3 */
-    //const CANADIAN_GALLON: f64 = (4.54609e-3) /* m^3 */
-    //const UK_GALLON: f64 = (4.546092e-3) /* m^3 */
-    //const MILES_PER_HOUR: f64 = (4.4704e-1) /* m / s */
+    /// Hour
+    const HOUR: Self;
+    /// Day
+    const DAY: Self;
+    /// Week
+    const WEEK: Self;
+    /// Meter
+    const METER: Self;
+    /// Inch
+    const INCH: Self;
+    /// Foot
+    const FOOT: Self;
+    /// Yard
+    const YARD: Self;
+    /// Mile
+    const MILE:Self;
+    /// Nautical mile
+    const NAUTICAL_MILE: Self;
+    /// Fathom
+    const FATHOM: Self;
+    /// Mil
+    const MIL: Self;
+    /// Point
+    const POINT: Self;
+    /// Textpoint
+    const TEXPOINT: Self;
+    /// Micron
+    const MICRON: Self;
+    /// Angstrom
+    const ANGSTROM: Self;
+    /// Hectare
+    const HECTARE: Self;
+    /// Acre
+    const ACRE: Self;
+    /// Barn
+    const BARN: Self;
+    /// Liter
+    const LITER: Self;
+    /// US gallon
+    const US_GALLON: Self;
+    /// Quart
+    const QUART: Self;
+    /// Pint
+    const PINT: Self;
+    /// Cup
+    const CUP: Self;
+    /// Fluid ounce
+    const FLUID_OUNCE: Self;
+    /// Tablespoon
+    const TABLESPOON: Self;
+    /// Teaspoon
+    const TEASPOON: Self;
+    /// Canadian gallon
+    const CANADIAN_GALLON: Self;
+    /// UK gallon
+    const UK_GALLON: Self;
+    /// miles/h
+    const MILES_PER_HOUR: Self;
     /// km/h
     const KILOMETERS_PER_HOUR: Self;
-    //const KNOT: f64 = (5.14444444444e-1) /* m / s */
-    //const POUND_MASS: f64 = (4.5359237e-1) /* kg */
-    //const OUNCE_MASS: f64 = (2.8349523125e-2) /* kg */
-    //const TON: f64 = (9.0718474e2) /* kg */
-    //const METRIC_TON: f64 = (1e3) /* kg */
-    //const UK_TON: f64 = (1.0160469088e3) /* kg */
-    //const TROY_OUNCE: f64 = (3.1103475e-2) /* kg */
-    //const CARAT: f64 = (2e-4) /* kg */
-    //const UNIFIED_ATOMIC_MASS: f64 = (1.660538782e-27) /* kg */
-    //const GRAM_FORCE: f64 = (9.80665e-3) /* kg m / s^2 */
-    //const POUND_FORCE: f64 = (4.44822161526e0) /* kg m / s^2 */
-    //const KILOPOUND_FORCE: f64 = (4.44822161526e3) /* kg m / s^2 */
-    //const POUNDAL: f64 = (1.38255e-1) /* kg m / s^2 */
-    //const CALORIE: f64 = (4.1868e0) /* kg m^2 / s^2 */
-    //const BTU: f64 = (1.05505585262e3) /* kg m^2 / s^2 */
-    //const THERM: f64 = (1.05506e8) /* kg m^2 / s^2 */
-    //const HORSEPOWER: f64 = (7.457e2) /* kg m^2 / s^3 */
-    //const BAR: f64 = (1e5) /* kg / m s^2 */
-    //const STD_ATMOSPHERE: f64 = (1.01325e5) /* kg / m s^2 */
-    //const TORR: f64 = (1.33322368421e2) /* kg / m s^2 */
-    //const METER_OF_MERCURY: f64 = (1.33322368421e5) /* kg / m s^2 */
-    //const INCH_OF_MERCURY: f64 = (3.38638815789e3) /* kg / m s^2 */
-    //const INCH_OF_WATER: f64 = (2.490889e2) /* kg / m s^2 */
-    //const PSI (6.89475729317e3) /* kg / m s^2 */
-    //const POISE (1e-1) /* kg m^-1 s^-1 */
-    //const STOKES (1e-4) /* m^2 / s */
-    //const STILB (1e4) /* cd / m^2 */
-    //const LUMEN (1e0) /* cd sr */
-    //const LUX (1e0) /* cd sr / m^2 */
-    //const PHOT (1e4) /* cd sr / m^2 */
-    //const FOOTCANDLE (1.076e1) /* cd sr / m^2 */
-    //const LAMBERT (1e4) /* cd sr / m^2 */
-    //const FOOTLAMBERT (1.07639104e1) /* cd sr / m^2 */
-    //const CURIE (3.7e10) /* 1 / s */
-    //const ROENTGEN (2.58e-4) /* A s / kg */
-    //const RAD (1e-2) /* m^2 / s^2 */
-    //const SOLAR_MASS (1.98892e30) /* kg */
-    //const BOHR_RADIUS (5.291772083e-11) /* m */
-    //const NEWTON (1e0) /* kg m / s^2 */
-    //const DYNE (1e-5) /* kg m / s^2 */
-    //const JOULE (1e0) /* kg m^2 / s^2 */
-    //const ERG (1e-7) /* kg m^2 / s^2 */
-    //const STEFAN_BOLTZMANN_CONSTANT (5.67040047374e-8) /* kg / K^4 s^3 */
-    //const THOMSON_CROSS_SECTION (6.65245893699e-29) /* m^2 */
-    //const BOHR_MAGNETON (9.27400899e-24) /* A m^2 */
-    //const NUCLEAR_MAGNETON (5.05078317e-27) /* A m^2 */
-    //const ELECTRON_MAGNETIC_MOMENT (9.28476362e-24) /* A m^2 */
-    //const PROTON_MAGNETIC_MOMENT (1.410606633e-26) /* A m^2 */
-    //const FARADAY (9.64853429775e4) /* A s / mol */
-    //const ELECTRON_CHARGE (1.602176487e-19) /* A s */
-    //const VACUUM_PERMITTIVITY (8.854187817e-12) /* A^2 s^4 / kg m^3 */
-    //const VACUUM_PERMEABILITY (1.25663706144e-6) /* kg m / A^2 s^2 */
-    //const DEBYE (3.33564095198e-30) /* A s^2 / m^2 */
-    //const GAUSS (1e-4) /* kg / A s^2 */
+    /// Knot
+    const KNOT: Self;
+    /// Kilogram
+    const KILOGRAM: Self;
+    //const POUND_MASS: Self;
+    //const OUNCE_MASS: Self;
+    //const TON: Self;
+    //const METRIC_TON: Self;
+    //const UK_TON: Self;
+    //const TROY_OUNCE: Self;
+    //const CARAT: Self;
+    //const UNIFIED_ATOMIC_MASS: Self;
+    //const GRAM_FORCE: Self;
+    //const POUND_FORCE: Self;
+    //const KILOPOUND_FORCE: Self;
+    //const POUNDAL: Self;
+    //const CALORIE: Self;
+    //const BTU: Self;
+    //const THERM: Self;
+    //const HORSEPOWER: Self;
+    //const BAR: Self;
+    //const STD_ATMOSPHERE: Self;
+    //const TORR: Self;
+    //const METER_OF_MERCURY: Self;
+    //const INCH_OF_MERCURY: Self;
+    //const INCH_OF_WATER: Self;
+    //const PSI Self;
+    //const POISE Self;
+    //const STOKES Self;
+    //const STILB Self;
+    //const LUMEN Self;
+    //const LUX Self;
+    //const PHOT Self;
+    //const FOOTCANDLE Self;
+    //const LAMBERT Self;
+    //const FOOTLAMBERT Self;
+    //const CURIE Self;
+    //const ROENTGEN Self;
+    //const RAD Self;
+    //const SOLAR_MASS Self;
+    //const BOHR_RADIUS Self;
+    //const NEWTON Self;
+    //const DYNE Self;
+    //const JOULE Self;
+    //const ERG Self;
+    //const STEFAN_BOLTZMANN_CONSTANT Self;
+    //const THOMSON_CROSS_SECTION Self;
+    //const BOHR_MAGNETON Self;
+    //const NUCLEAR_MAGNETON Self;
+    //const ELECTRON_MAGNETIC_MOMENT Self;
+    //const PROTON_MAGNETIC_MOMENT Self;
+    //const FARADAY Self;
+    //const ELECTRON_CHARGE Self;
+    //const VACUUM_PERMITTIVITY Self;
+    //const VACUUM_PERMEABILITY Self;
+    //const DEBYE Self;
+    //const GAUSS Self;
 }
 
 impl Mks for f64 {
@@ -591,37 +630,40 @@ impl Mks for f64 {
     const BOLTZMANN:                f64 = 1.3806504e-23_f64; /* kg m^2 / K s^2 */
     const MOLAR_GAS:                f64 = 8.314472e0_f64; /* kg m^2 / K mol s^2 */
     const STANDARD_GAS_VOLUME:      f64 = 2.2710981e-2_f64; /* m^3 / mol */
+    const SECOND:                   f64 = 1.0_f64; // s
     const MINUTE:                   f64 = 6.0e1_f64; // s
-    //const HOUR:                   f64 = (3.6e3) /* s */
-    //const DAY:                    f64 = (8.64e4) /* s */
-    //const WEEK:                   f64 = (6.048e5) /* s */
-    //const INCH:                   f64 = (2.54e-2) /* m */
-    //const FOOT:                   f64 = (3.048e-1) /* m */
-    //const YARD:                   f64 = (9.144e-1) /* m */
-    //const MILE:                   f64 = (1.609344e3) /* m */
-    //const NAUTICAL_MILE: f64 = (1.852e3) /* m */
-    //const FATHOM: f64 = (1.8288e0) /* m */
-    //const MIL: f64 = (2.54e-5) /* m */
-    //const POINT: f64 = (3.52777777778e-4) /* m */
-    //const TEXPOINT: f64 = (3.51459803515e-4) /* m */
-    //const MICRON: f64 = (1e-6) /* m */
-    //const ANGSTROM: f64 = (1e-10) /* m */
-    //const HECTARE: f64 = (1e4) /* m^2 */
-    //const ACRE: f64 = (4.04685642241e3) /* m^2 */
-    //const BARN: f64 = (1e-28) /* m^2 */
-    //const LITER: f64 = (1e-3) /* m^3 */
-    //const US_GALLON: f64 = (3.78541178402e-3) /* m^3 */
-    //const QUART: f64 = (9.46352946004e-4) /* m^3 */
-    //const PINT: f64 = (4.73176473002e-4) /* m^3 */
-    //const CUP: f64 = (2.36588236501e-4) /* m^3 */
-    //const FLUID_OUNCE: f64 = (2.95735295626e-5) /* m^3 */
-    //const TABLESPOON: f64 = (1.47867647813e-5) /* m^3 */
-    //const TEASPOON: f64 = (4.92892159375e-6) /* m^3 */
-    //const CANADIAN_GALLON: f64 = (4.54609e-3) /* m^3 */
-    //const UK_GALLON: f64 = (4.546092e-3) /* m^3 */
-    //const MILES_PER_HOUR: f64 = (4.4704e-1) /* m / s */
-    const KILOMETERS_PER_HOUR: f64 = 2.77777777778e-1; // m / s
-    //const KNOT: f64 = (5.14444444444e-1) /* m / s */
+    const HOUR:                     f64 = 3.6e3_f64; // s
+    const DAY:                      f64 = 8.64e4_f64; // s
+    const WEEK:                     f64 = 6.048e5_f64; // s
+    const METER:                    f64 = 1.0_f64; // m
+    const INCH:                     f64 = 2.54e-2_f64; /* m */
+    const FOOT:                     f64 = 3.048e-1_f64; /* m */
+    const YARD:                     f64 = 9.144e-1_f64; /* m */
+    const MILE:                     f64 = 1.609344e3_f64; /* m */
+    const NAUTICAL_MILE:            f64 = 1.852e3_f64; /* m */
+    const FATHOM:                   f64 = 1.8288e0_f64; /* m */
+    const MIL:                      f64 = 2.54e-5_f64; /* m */
+    const POINT:                    f64 = 3.52777777778e-4_f64; /* m */
+    const TEXPOINT:                 f64 = 3.51459803515e-4_f64; /* m */
+    const MICRON:                   f64 = 1e-6_f64; /* m */
+    const ANGSTROM:                 f64 = 1e-10_f64; /* m */
+    const HECTARE:                  f64 = 1e4_f64; /* m^2 */
+    const ACRE:                     f64 = 4.04685642241e3_f64; /* m^2 */
+    const BARN:                     f64 = 1e-28_f64; /* m^2 */
+    const LITER:                    f64 = 1e-3_f64; /* m^3 */
+    const US_GALLON:                f64 = 3.78541178402e-3_f64; /* m^3 */
+    const QUART:                    f64 = 9.46352946004e-4_f64; /* m^3 */
+    const PINT:                     f64 = 4.73176473002e-4_f64; /* m^3 */
+    const CUP:                      f64 = 2.36588236501e-4_f64; /* m^3 */
+    const FLUID_OUNCE:              f64 = 2.95735295626e-5_f64; /* m^3 */
+    const TABLESPOON:               f64 = 1.47867647813e-5_f64; /* m^3 */
+    const TEASPOON:                 f64 = 4.92892159375e-6_f64; /* m^3 */
+    const CANADIAN_GALLON:          f64 = 4.54609e-3_f64; /* m^3 */
+    const UK_GALLON:                f64 = 4.546092e-3_f64; /* m^3 */
+    const MILES_PER_HOUR:           f64 = 4.4704e-1_f64; /* m / s */
+    const KILOMETERS_PER_HOUR:      f64 = 2.77777777778e-1_f64; // m / s
+    const KNOT:                     f64 = 5.14444444444e-1_f64; /* m / s */
+    const KILOGRAM:                 f64 = 1.0_f64; // kg
     //const POUND_MASS: f64 = (4.5359237e-1) /* kg */
     //const OUNCE_MASS: f64 = (2.8349523125e-2) /* kg */
     //const TON: f64 = (9.0718474e2) /* kg */
