0a7c43e87d0b2751786be0f42afd4962aa910b92
Igor Lesik <ilesik@tachyum.com>
2023-04-05 22:55:51 -0700

    working on equations

--- src/physics.rs
+++ src/physics.rs
@@ -22,15 +22,67 @@ pub struct EqParams<const NR_OUT: usize, const NR_CONST: usize, const NR_IN: usi
     pub inp: [MksUnit; NR_IN]
 }
 
-/// Equation interface
-pub trait Equation {
+/// Equation creation interface
+pub trait EquationMaker {
     /// Return tuple with paramerts type
     fn params() -> (&'static [MksUnit], &'static [MksUnit], &'static [MksUnit]);
 
-    /// Create new equation with constant parameters provided.
-    fn make(cns: &[f64]) -> Self;
+    /// Create new equation with provided constant parameters.
+    fn make(cns: &[f64]) -> Box<dyn Equation>;
+}
 
-    /// Create new equation with constant parameters provided.
+/// Equation interface
+pub trait Equation {
+    /// Run equation with provided input parameters.
     fn run(&mut self, inp: &[f64]) -> Vec<f64>;
 }
 
+/// Record about an equation.
+///
+/// 1st function returns `tuple(output, constant, input : &[MksUnit])`.
+///
+pub type BuildTuple<'a> = (
+    &'a str,
+    fn () -> (&'static [MksUnit], &'static [MksUnit], &'static [MksUnit]),
+    fn (&[f64]) -> Box<dyn Equation>,
+);
+
+/// List/array of all equations.
+pub const EQUATIONS: [BuildTuple; 4] = [
+    ("Linear motion const accel velocity `v = v0 + a*t`",
+    mechanics::linear_motion::const_accel::VelocityEquation::params,
+    mechanics::linear_motion::const_accel::VelocityEquation::make),
+    ("Linear motion const accel velocity `v = sqrt(v0^2 + 2*a*s)`",
+    mechanics::linear_motion::const_accel::VelocityByDistEquation::params,
+    mechanics::linear_motion::const_accel::VelocityByDistEquation::make),
+    ("Linear motion const accel distance `s = v0*t + (a*t^2)/2`",
+    mechanics::linear_motion::const_accel::DistanceEquation::params,
+    mechanics::linear_motion::const_accel::DistanceEquation::make),
+    ("Linear motion const accel distance `s = t*(v0 + v)/2`",
+    mechanics::linear_motion::const_accel::DistanceByVelEquation::params,
+    mechanics::linear_motion::const_accel::DistanceByVelEquation::make),
+];
+
+/// Get list of equations that have specified input/output unit types.
+///
+/// # Example
+///
+/// ```
+/// use rustamath::physics::{find_equation_by_units, Equation, EquationMaker, EQUATIONS};
+/// use rustamath_mks::*;
+/// let ids = find_equation_by_units(&[TIME_UNIT], &[VELOCITY_UNIT]);
+/// assert_eq!(ids[0], 0);
+/// let mut equation = EQUATIONS[ids[0]].2(&[3.0, 2.0]);
+/// assert_eq!(equation.run(&[10.0])[0], 3.0 + 2.0*10.0);
+/// ```
+pub fn find_equation_by_units(inputs: &[MksUnit], outputs: &[MksUnit]) -> Vec<usize> {
+    let mut eqs: Vec<usize> = Vec::new();
+
+    for (index, eq) in EQUATIONS.iter().enumerate() {
+        let (out, _cns, inp) = eq.1();
+        if out == outputs && inp == inputs {
+            eqs.push(index);
+        }
+    }
+    eqs
+}
--- src/physics/mechanics/linear_motion/const_accel.rs
+++ src/physics/mechanics/linear_motion/const_accel.rs
@@ -8,7 +8,7 @@
 //! - <https://en.wikipedia.org/wiki/List_of_equations_in_classical_mechanics>
 //!
 use rustamath_mks::*;
-use super::super::super::{EqParams, Equation};
+use super::super::super::{EqParams, Equation, EquationMaker};
 
 /// Velocity formula parameters type
 pub const VELOCITY_EQ_PARAMS: EqParams<1, 2, 1> = EqParams {
@@ -66,14 +66,14 @@ impl VelocityEquation {
     }
 }
 
-impl Equation for VelocityEquation {
+impl EquationMaker for VelocityEquation {
     /// Get parameters type.
     ///
     /// # Example
     ///
     /// ```
     /// use rustamath::physics::mechanics::linear_motion::const_accel::VelocityEquation;
-    /// use rustamath::physics::Equation;
+    /// use rustamath::physics::{Equation, EquationMaker};
     /// use rustamath_mks::*;
     /// let params = VelocityEquation::params();
     /// assert!(params.0 == &[VELOCITY_UNIT]);
@@ -84,20 +84,119 @@ impl Equation for VelocityEquation {
     }
 
     /// Create new equation with constant parameters provided.
-    fn make(cns: &[f64]) -> Self {
-        VelocityEquation::new(cns[0], cns[1])
+    fn make(cns: &[f64]) -> Box<dyn Equation> {
+        Box::new(VelocityEquation::new(cns[0], cns[1]))
     }
+}
 
-    /// Create new equation with constant parameters provided.
+impl Equation for VelocityEquation {
+    /// Run equation with inputs provided.
     ///
     /// # Example
     ///
     /// ```
     /// use rustamath::physics::mechanics::linear_motion::const_accel::VelocityEquation;
-    /// use rustamath::physics::Equation;
+    /// use rustamath::physics::{Equation, EquationMaker};
     /// let mut eq = VelocityEquation::make(&[2.0, 3.0]);
     /// let res = eq.run(&[10.0]);
     /// assert_eq!(res[0], 32.0);
+    /// ```
+    fn run(&mut self, inp: &[f64]) -> Vec<f64> {
+        self.calc(inp[0]);
+        vec![self.velocity.val]
+    }
+}
+
+/// Velocity-by-distance formula parameters type
+pub const VELOCITY_BY_DIST_EQ_PARAMS: EqParams<1, 2, 1> = EqParams {
+    out: [VELOCITY_UNIT], cns: [VELOCITY_UNIT, ACCEL_UNIT], inp: [DISTANCE_UNIT]};
+
+/// Velocity-by-distance equation
+pub struct VelocityByDistEquation {
+    /// Velocity `v = sqrt(v0^2 + 2*a*s)`.
+    pub velocity: MksVal,
+    /// Initial velocity
+    pub initial_velocity: MksVal,
+    /// Constant acceleration
+    pub acceleration: MksVal,
+    /// Distance
+    pub distance: MksVal,
+}
+
+impl VelocityByDistEquation {
+    /// Parameters type
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rustamath::physics::mechanics::linear_motion::const_accel::VelocityByDistEquation;
+    /// use rustamath_mks::*;
+    /// assert!(VelocityByDistEquation::PARAMS.out == [VELOCITY_UNIT]);
+    /// assert!(VelocityByDistEquation::PARAMS.inp == [DISTANCE_UNIT]);
+    /// ```
+    pub const PARAMS: EqParams<1, 2, 1> = VELOCITY_BY_DIST_EQ_PARAMS;
+
+    /// Initialize constants
+    pub fn new(v0: f64, a: f64) -> VelocityByDistEquation {
+        VelocityByDistEquation {
+            velocity: MksVal {val: 0.0, unit: VELOCITY_UNIT},
+            initial_velocity: MksVal {val: v0, unit: VELOCITY_UNIT},
+            acceleration: MksVal {val: a, unit: ACCEL_UNIT},
+            distance: MksVal {val: 0.0, unit: DISTANCE_UNIT},
+        }
+    }
+
+    /// Calculate velocity by distance with constant acceleration.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rustamath::physics::mechanics::linear_motion::const_accel::VelocityByDistEquation;
+    /// let mut eq = VelocityByDistEquation::new(3.0, 4.0);
+    /// eq.calc(2.0);
+    /// assert_eq!(eq.velocity.val, (3.0*3.0f64 + 2.0*4.0*2.0f64).sqrt());
+    /// ```
+    pub fn calc(&mut self, d: f64) {
+        self.distance.val = d;
+        self.velocity = ((self.initial_velocity * self.initial_velocity) +
+            (MksVal::new_scalar(2.0) * self.acceleration * self.distance)).sqrt();
+    }
+}
+
+impl EquationMaker for VelocityByDistEquation {
+    /// Get parameters type.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rustamath::physics::mechanics::linear_motion::const_accel::VelocityByDistEquation;
+    /// use rustamath::physics::EquationMaker;
+    /// use rustamath_mks::*;
+    /// let params = VelocityByDistEquation::params();
+    /// assert!(params.0 == &[VELOCITY_UNIT]);
+    /// assert!(params.2 == &[DISTANCE_UNIT]);
+    /// ```
+    fn params() -> (&'static [MksUnit], &'static [MksUnit], &'static [MksUnit]) {
+        (&Self::PARAMS.out, &Self::PARAMS.cns, &Self::PARAMS.inp)
+    }
+
+    /// Create new equation with constant parameters provided.
+    fn make(cns: &[f64]) -> Box<dyn Equation> {
+        Box::new(VelocityByDistEquation::new(cns[0], cns[1]))
+    }
+}
+
+impl Equation for VelocityByDistEquation {
+    /// Run equation with inputs provided.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rustamath::physics::mechanics::linear_motion::const_accel::VelocityByDistEquation;
+    /// use rustamath::physics::{Equation, EquationMaker};
+    /// let mut eq = VelocityByDistEquation::make(&[3.0, 4.0]);
+    /// let res = eq.run(&[2.0]);
+    /// assert_eq!(res[0], (3.0*3.0f64 + 2.0*4.0*2.0f64).sqrt());
     /// ```
     fn run(&mut self, inp: &[f64]) -> Vec<f64> {
         self.calc(inp[0]);
@@ -162,14 +261,14 @@ impl DistanceEquation {
     }
 }
 
-impl Equation for DistanceEquation {
+impl EquationMaker for DistanceEquation {
     /// Get parameters type.
     ///
     /// # Example
     ///
     /// ```
     /// use rustamath::physics::mechanics::linear_motion::const_accel::DistanceEquation;
-    /// use rustamath::physics::Equation;
+    /// use rustamath::physics::{Equation, EquationMaker};
     /// use rustamath_mks::*;
     /// let params = DistanceEquation::params();
     /// assert!(params.0 == &[DISTANCE_UNIT]);
@@ -180,20 +279,120 @@ impl Equation for DistanceEquation {
     }
 
     /// Create new equation with constant parameters provided.
-    fn make(cns: &[f64]) -> Self {
-        DistanceEquation::new(cns[0], cns[1])
+    fn make(cns: &[f64]) -> Box<dyn Equation> {
+        Box::new(DistanceEquation::new(cns[0], cns[1]))
     }
+}
 
-    /// Create new equation with constant parameters provided.
+impl Equation for DistanceEquation {
+    /// Run equation with inputs provided.
     ///
     /// # Example
     ///
     /// ```
     /// use rustamath::physics::mechanics::linear_motion::const_accel::DistanceEquation;
-    /// use rustamath::physics::Equation;
+    /// use rustamath::physics::{Equation, EquationMaker};
     /// let mut eq = DistanceEquation::make(&[2.0, 3.0]);
     /// let res = eq.run(&[10.0]);
     /// assert_eq!(res[0], (2.0 * 10.0) + (3.0 * 100.0)/2.0);
+    /// ```
+    fn run(&mut self, inp: &[f64]) -> Vec<f64> {
+        self.calc(inp[0]);
+        vec![self.distance.val]
+    }
+}
+
+/// Distance-by-velocity formula parameters type
+pub const DISTANCE_BY_VEL_EQ_PARAMS: EqParams<1, 2, 1> = EqParams {
+    out: [DISTANCE_UNIT], cns: [VELOCITY_UNIT, VELOCITY_UNIT], inp: [TIME_UNIT]};
+
+/// Distance-by-velocity equation
+pub struct DistanceByVelEquation {
+    /// Distance `s = t*(v0 + v)/2`.
+    pub distance: MksVal,
+    /// Initial velocity
+    pub initial_velocity: MksVal,
+    /// Final velocity
+    pub final_velocity: MksVal,
+    /// Time
+    pub time: MksVal,
+}
+
+impl DistanceByVelEquation {
+    /// Parameters type
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rustamath::physics::mechanics::linear_motion::const_accel::DistanceByVelEquation;
+    /// use rustamath_mks::*;
+    /// assert!(DistanceByVelEquation::PARAMS.out == [DISTANCE_UNIT]);
+    /// assert!(DistanceByVelEquation::PARAMS.cns == [VELOCITY_UNIT, VELOCITY_UNIT]);
+    /// ```
+    pub const PARAMS: EqParams<1, 2, 1> = DISTANCE_BY_VEL_EQ_PARAMS;
+
+    /// Initialize constants
+    pub fn new(v0: f64, v: f64) -> DistanceByVelEquation {
+        DistanceByVelEquation {
+            distance: MksVal {val: 0.0, unit: DISTANCE_UNIT},
+            initial_velocity: MksVal {val: v0, unit: VELOCITY_UNIT},
+            final_velocity: MksVal {val: v, unit: VELOCITY_UNIT},
+            time: MksVal {val: 0.0, unit: TIME_UNIT},
+        }
+    }
+
+    /// Calculate distance by time with constant acceleration.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rustamath::physics::mechanics::linear_motion::const_accel::DistanceByVelEquation;
+    /// let mut eq = DistanceByVelEquation::new(2.0, 3.0);
+    /// eq.calc(10.0);
+    /// assert_eq!(eq.distance.val, (50.0)/2.0);
+    /// ```
+    pub fn calc(&mut self, t: f64) {
+        self.time.val = t;
+        self.distance =
+            (self.initial_velocity + self.final_velocity) *
+            self.time * MksVal::new_scalar(0.5);
+    }
+}
+
+impl EquationMaker for DistanceByVelEquation {
+    /// Get parameters type.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rustamath::physics::mechanics::linear_motion::const_accel::DistanceByVelEquation;
+    /// use rustamath::physics::{Equation, EquationMaker};
+    /// use rustamath_mks::*;
+    /// let params = DistanceByVelEquation::params();
+    /// assert!(params.0 == &[DISTANCE_UNIT]);
+    /// assert!(params.1 == &[VELOCITY_UNIT, VELOCITY_UNIT]);
+    /// ```
+    fn params() -> (&'static [MksUnit], &'static [MksUnit], &'static [MksUnit]) {
+        (&Self::PARAMS.out, &Self::PARAMS.cns, &Self::PARAMS.inp)
+    }
+
+    /// Create new equation with constant parameters provided.
+    fn make(cns: &[f64]) -> Box<dyn Equation> {
+        Box::new(DistanceByVelEquation::new(cns[0], cns[1]))
+    }
+}
+
+impl Equation for DistanceByVelEquation {
+    /// Run equation with inputs provided.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rustamath::physics::mechanics::linear_motion::const_accel::DistanceByVelEquation;
+    /// use rustamath::physics::{Equation, EquationMaker};
+    /// let mut eq = DistanceByVelEquation::make(&[2.0, 3.0]);
+    /// let res = eq.run(&[10.0]);
+    /// assert_eq!(res[0], (50.0)/2.0);
     /// ```
     fn run(&mut self, inp: &[f64]) -> Vec<f64> {
         self.calc(inp[0]);
