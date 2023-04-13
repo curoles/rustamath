39773789d15911ed660e8f0f2db70db8df7f5d30
Igor Lesik <ilesik@tachyum.com>
2023-04-13 15:40:27 -0700

    remove physics

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ extern crate assert_float_eq;
 
 pub mod simd;
 pub mod constant;
-pub mod physics;
 pub mod function;
 pub mod roots;
 pub mod polynomial;
--- src/physics/function.rs
+++
@@ -1,6 +0,0 @@
-//! Unit-less functions.
-//!
-//! (c) 2023 Igor Lesik
-//! MIT license
-//!
-pub mod sin;
